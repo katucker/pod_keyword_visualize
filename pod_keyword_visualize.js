9f18bf21ea99d055d5116edef28bb094f68a0303
Keith Tucker <tuck220@icloud.com>
2017-07-09 20:42:32 -0400

    Corrected setting initial radius for nodes.

--- pod_keyword_visualize.js
+++ pod_keyword_visualize.js
@@ -5,9 +5,57 @@
  * This version supports schema version 1.1.
 */
 
+// Polyfill the Array find function for older Javascript engines that don't
+// include it natively.
+// https://tc39.github.io/ecma262/#sec-array.prototype.find
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+     // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    }
+  });
+}
+
 var NUMBER_INITIAL_KEYWORDS = 20;
-var PADDING = 5;
-var ITERATIONS = 16;
+var PADDING = 50;
+var ITERATIONS = 4;
+var REPULSION = -150;
 
 var keywords = { nodes: [] };
 var displayNodes = [];
@@ -33,29 +81,29 @@ var areaScale = d3.scaleSqrt();
 
 // Function for repositioning the edges and nodes based on the simulation, below.
 function ticked() {
-  edge
-    .attr("x1", function(d) { return d.source.x; })
-    .attr("y1", function(d) { return d.source.y; })
-    .attr("x2", function(d) { return d.target.x; })
-    .attr("y2", function(d) { return d.target.y; });
+  var tickNodes = svg.selectAll(".node");
+  var tickEdges = svg.selectAll(".edge");
+  
+  tickEdges.attr("x1", function(d) { return d.source.x; })
+           .attr("y1", function(d) { return d.source.y; })
+           .attr("x2", function(d) { return d.target.x; })
+           .attr("y2", function(d) { return d.target.y; });
     
-  node
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; });
+  tickNodes.attr("cx", function(d) { return d.x; })
+           .attr("cy", function(d) { return d.y; });
 }
     
-// Create a force simulation for positioning the keyword nodes and links.
+// Create a force simulation for positioning the keyword nodes and edges.
 // Stop the automatic run of the simulation since there is no data yet.
 var forceMap = d3.forceSimulation()
-                 .force("collide", 
-                        d3.forceCollide(function(d) {
-                          return areaScale(d.datasetCount);
-                        }).iterations(ITERATIONS))
-                 .force("link", d3.forceLink().id(function (d) { return d.id; }))
-                 .force("charge", d3.forceManyBody())
-                 .force("center", d3.forceCenter(width/2, height/2))
-                 .on("tick",ticked)
-                 .stop();
+                 .force("collide", d3.forceCollide().iterations(ITERATIONS))
+                 .force("link", d3.forceLink()
+                                    .id( function(d) { return d.id; }))
+                 .force("charge", d3.forceManyBody().strength(REPULSION))
+//                 .force("center", d3.forceCenter(width/2, height/2))
+                 .force("xPos", d3.forceX())
+                 .force("yPos", d3.forceY())
+                 .on("tick",ticked);
     
 function buildSelectionList() {
   // Build a selection list for the aggregated keywords. Remove any
@@ -106,7 +154,8 @@ function displayForceMap() {
     for (var j = 0; j < displayNodes[i].connections.length; j++) {
       var target = displayNodes.find( function (d) { return d.id === displayNodes[i].connections[j]; });
       if (typeof target !== "undefined") {
-        displayEdges.push({ source: displayNodes[i].id, target: displayNodes[i].connections[j] });
+        displayEdges.push({ source: displayNodes[i].id, 
+                            target: displayNodes[i].connections[j] });
       }
     }
   }
@@ -118,41 +167,56 @@ function displayForceMap() {
   // Set the area scale range to ensure the circles can all fit within
   // the width and height of the svg area without overlapping.
   areaScale.range([1,d3.min([(width/displayNodes.length), (height/displayNodes.length)])]);
-  
-  node = svg.select(".nodes")
-                .selectAll("circle")
-                .data(displayNodes, function(d) { return d ? d.id : this.id; });
-  
-  // Remove any nodes no longer in the map.              
-  node.exit().remove();
 
-  // Create a circle for each keyword in the filtered subset.
-  node.enter().append("circle")
-                .attr("r", function (d) { return areaScale(d.datasetCount); })
-                .attr("id", function(d) { return d.id; })
-      .merge(node)
-      .call(d3.drag()
-        .on("start",dragStarted)
-        .on("drag",dragged)
-        .on("end",dragEnded));
-
-  // Add the keyword name as the title of each circle.
-  node.append("title").text(function(d) { return d.id });
-
-  // Create lines between the circles for the keyword connections.
-  edge = svg.select(".edges")
-                 .selectAll("line")
-                 .data(displayEdges);
+  // Retrieve the list of nodes, set it to the new data set and adjust any existing radii.  
+  node = svg.selectAll(".node")
+            .data(displayNodes, function(d) { return d ? d.id : this.id; })
+            .attr("r", function (d) { return areaScale(d.datasetCount); });
+  // Retrieve the list of edges between nodes, and set it to the new data.
+  edge = svg.selectAll(".edge")
+            .data(displayEdges);
                  
+  // Remove any nodes and edges that should no longer be displayed.              
+  node.exit().remove();
   edge.exit().remove();
   
-  edge.enter().append("line")
-    .merge(edge)
+  // Create a circle for each keyword in the filtered subset and set the
+  // title to the keyword.
+  node.enter().append("circle")
+                .classed("node", true)
+                .attr("id", function(d) { return d.id; })
+                .attr("r", function (d) { return areaScale(d.datasetCount); })
+                .merge(node)
+                   .call(d3.drag()
+                           .on("start",dragStarted)
+                           .on("drag",dragged)
+                           .on("end",dragEnded))
+                   .append("title").text(function(d) { return d.id });
+                
+                
+  // Create lines between the circles for the keyword connections.
+  edge.enter().insert("line", ".node").classed("edge", true);
 
+  // Compute the optimal value to use for the link distance, such that all the selected
+  // nodes should fit within the display area.
+  var dist = (Math.min(width,height) / (displayNodes.length ? displayNodes.length : 1))
+    + PADDING;
+  
   // Load the nodes and edges into the force map.
   forceMap.nodes(displayNodes);
-  forceMap.force("link").links(displayEdges);
-
+  
+  // Set the collision force to use the updated radius of each node.
+  forceMap.force("collide").radius(function(d) { return areaScale(d.datasetCount); });
+  
+  // Load the edges into the link force, and set it to use the new names and updated
+  // distance value.
+  forceMap.force("link").links(displayEdges)
+                        .distance(dist);
+                        
+  // Initialize each node to a random position near the center of the graph area.
+  forceMap.force("xPos").x(function (d) { return width/2 + Math.random() });
+  forceMap.force("yPos").y(function (d) { return height/2 + Math.random() });
+  
   // Restart the simulation on the new data.
   forceMap.alpha(1).restart();
 
@@ -268,7 +332,7 @@ function toggleKeyword(i) {
 }
 
 //Initialize DOM elements within the SVG element for the nodes and edges.
-svg.append("g").attr("class","edges");
-svg.append("g").attr("class","nodes");
+//svg.append("g").attr("class","edges");
+//svg.append("g").attr("class","nodes");
 
 document.getElementById("map_button").onclick = mapKeywords;
